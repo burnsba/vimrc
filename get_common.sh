666d72ca7b522fc378ae3e89b36223a86766816d
Ben Burns <benjaminaburns@gmail.com>
2015-12-10 17:54:15 -0600

    Update get_common.sh

--- get_common.sh
+++ get_common.sh
@@ -1,4 +1,10 @@
 #!/binsh
 cd ~
+if [ -f ".screenrc" ]; then
+    mv .screenrc .screenrc_backup
+fi
+if [ -f ".vimrc" ]; then
+    mv .vimrc .vimrc_backup
+fi
 wget https://raw.githubusercontent.com/burnsba/vimrc/master/.screenrc
 wget https://raw.githubusercontent.com/burnsba/vimrc/master/.vimrc
